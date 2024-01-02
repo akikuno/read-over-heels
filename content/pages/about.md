15c0a4b29f299bd364bfb5b787ccb2a9396a26ef
Akihiro Kuno <akikuno@users.noreply.github.com>
2024-01-02 19:24:26 +0900

    update pelicanconf

---
+++ content/pages/about.md
@@ -0,0 +1,10 @@
+Title: About
+Summary: Brief infomation about me
+Author: akikuno
+Date: 2024-01-01
+Modified: 2024-01-01
+
+Hi there!
+
+I'm [Akihiro Kuno](), a software engineer in Automotive field.
+`Simplify` is my first theme I created for my own blog. Please feel free to use this theme for your blogs too.
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,14 +1,20 @@
 AUTHOR = 'akikuno'
 SITENAME = 'Read Over Heels'
-SITEURL = ""
+SITEURL = 'https://akikuno.github.io/read-over-heels'
+TIMEZONE = 'Asia/Tokyo'
+DEFAULT_LANG = 'ja'
+DATE_FORMATS = {'jp': '%Y-%m-%d'}
 
 PATH = "content"
-
-TIMEZONE = 'Asia/Tokyo'
-
-DEFAULT_LANG = 'ja'
-
 THEME = "./pelican-themes/simplify-theme"
+
+ARTICLE_PATHS = ['blog']
+ARTICLE_URL = 'blog/{slug}.html'
+ARTICLE_SAVE_AS = 'blog/{slug}.html'
+ARTICLE_PRIMARY_PATH = 'blog'
+
+PAGE_PATHS = ['pages']
+
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -17,11 +23,11 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# SNS周りの設定
-SOCIAL = (
-    ('twitter', 'https://twitter.com/akicuno'),
-    ('gitHub', 'https://github.com/akikuno'),
-)
+# Social widget
+SOCIAL = {
+    'github': 'https://github.com/akikuno',
+    'twitter': 'https://twitter.com/akicuno',
+}
 
 DEFAULT_PAGINATION = 10
 
@@ -37,13 +43,6 @@ MARKDOWN = {
   'output_format': 'html5',
 }
 
-SITEURL = 'https://akikuno.github.io/read-over-heels'
 
-
-# ページ下部にライセンス表記を追加
-COPYRIGHT_NAME = "akikuno"
-CC_LICENSE = {
-    "name": "Creative Commons Attribution-ShareAlike",
-    "version": "4.0",
-    "slug": "by-sa"
-}
+RELATED_POSTS_MAX = 5
+RELATED_POSTS_SKIP_SAME_CATEGORY = False
